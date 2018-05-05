1e40ed0748b0a8f24d653e25b218ed110362b0ff
julia sheremet <juliajessicasher@gmail.com>
2018-05-05 15:33:16 -0700

    link app-homepage to appcomponents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Article } from './models/object.model';
 import { HomepageComponent } from './homepage/homepage.component';
 
@@ -8,12 +8,21 @@ import { HomepageComponent } from './homepage/homepage.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Kinfolk';
+  articles: Article[] = [
+    new Article('Studio David Thulstrup', 'The Danish designer behind Noma’s new interiors.', 'https://kinfolk.com/wp-content/uploads/2018/04/Kinfolk_StudioDavidThulstrup_009-683x1024.jpg'),
+    new Article('Métro, Boulot, Disco', 'The enduring appeal of Nouvelle Vague.', 'https://kinfolk.com/wp-content/uploads/2018/02/Metro_Boulet_Disco_01-768x1024.jpg'),
+    new Article('Septime', 'A Parisian restaurant serving up haute-cuisine without the pretention.','https://kinfolk.com/wp-content/uploads/2018/04/Kinfolk_ParisCityGuide_Septime_002-683x1024.jpg'),
+    new Article('Michaela DePrince', 'The young ballerina dancing all over the stereotypes of a pressure-intense career.', 'https://kinfolk.com/wp-content/uploads/2018/02/Michaela_DePrince_01-822x1024.jpg'),
+    new Article('The Photographer in the Garden', 'Gardens have provided powerful inspiration for Man Ray, Mapplethorpe and a million hobby photographers.', 'https://kinfolk.com/wp-content/uploads/2018/04/The_Photographer_in_the_Garden_04-851x1024.jpg'),
+    new Article('13 Bonaparte', 'Prize-winning photographer Allyssa Heuze on her collaboration with 13 Bonaparte.', 'https://kinfolk.com/wp-content/uploads/2018/05/13-Bonaparte-Le-Denim-2018-03-819x1024.jpg'),
+    new Article('Jamieson Webster', 'A conversation with a New York psychoanalyst.', 'https://kinfolk.com/wp-content/uploads/2018/01/Jamieson_Webster-819x1024.jpg'),
+    new Article('Red Heads', 'Red hair don’t care: Celebrating the beauty of the ginger gene.', 'https://kinfolk.com/wp-content/uploads/2018/01/Red_Heads_01-683x1024.jpg')
+  ];
 
-  // selectedArticle = null;
-  //
-  // viewArticle(clickedTitle){
-  //   console.log(clickedTitle.description);
-  //   this.selectedArticle = clickedTitle;
-  // }
+  selectedArticle = null;
+
+  viewArticle(clickedTitle){
+    console.log(clickedTitle.description);
+    this.selectedArticle = clickedTitle;
+  }
 }
