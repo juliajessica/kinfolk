13a8d19ad5313d4652d5d1e20785c178d45dbfe6
julia sheremet <juiajessicasher@gmail.com>
2018-05-08 19:18:19 -0700

    add functions to open and close the navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,6 +9,18 @@ export class NavbarComponent implements OnInit {
 
   constructor() { }
 
+openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+// }
+
   ngOnInit() {
   }
 
