b22c1fe86159c666ac78e046279c3c0faf6cac17
julia sheremet <juiajessicasher@gmail.com>
2018-05-11 10:20:16 -0700

    add email functionality to take user email

--- src/app/email/email.component.ts
+++ src/app/email/email.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ArticleService } from '../article.service';
 
 @Component({
   selector: 'app-email',
   templateUrl: './email.component.html',
-  styleUrls: ['./email.component.css']
+  styleUrls: ['./email.component.css'],
+  providers: [ArticleService]
 })
 export class EmailComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private albumService: ArticleService) { }
+
+  submitForm(userInput){
+    if (userInput === true){
+      document.getElementById("errorResponse").innerHTML = "Thank you!";
+    }
+    else {
+      document.getElementById("successResponse").innerHTML = "You have entered an invalid email address. Please try again.";
+    }
+  }
+
+
 
   ngOnInit() {
   }
