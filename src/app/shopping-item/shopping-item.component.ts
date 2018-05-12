530db889705a81dad7cfe1c901e14702e90bdba3
julia sheremet <juiajessicasher@gmail.com>
2018-05-11 18:15:52 -0700

    import and add functionality to shopping-item component

--- src/app/shopping-item/shopping-item.component.ts
+++ src/app/shopping-item/shopping-item.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Shop } from '../models/shop.model';
+import { FirebaseObjectObservable } from 'angularfire2/database';
+import { ShopService } from '../shop.service';
 
 @Component({
   selector: 'app-shopping-item',
   templateUrl: './shopping-item.component.html',
-  styleUrls: ['./shopping-item.component.css']
+  styleUrls: ['./shopping-item.component.css'],
+  providers: [ShopService]
 })
 export class ShoppingItemComponent implements OnInit {
+  itemId: string;
+  itemToDisplay;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location, private shopService: ShopService) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.itemId = urlParameters['id'];
+    });
+    this.itemToDisplay = this.shopService.findItemforSaleById(this.itemId);
   }
-
 }
